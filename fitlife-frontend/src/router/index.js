004e3b0231e63134ad57b142f7eed78cdcdaa3ad
Jan Samuel G. Sultan <jgsultan@student.apc.edu.ph>
2026-02-12 20:55:12 +0000

    updated router

--- fitlife-frontend/src/router/index.js
+++ fitlife-frontend/src/router/index.js
@@ -1,28 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router"
-import { useAuthStore } from "@/store/auth"
-
-import Login from "@/views/auth/Login.vue"
-import AdminDashboard from "@/views/admin/AdminDashboard.vue"
-import MemberDashboard from "@/views/member/MemberDashboard.vue"
-
-const routes = [
-  { path: "/", component: Login },
-  { path: "/admin", component: AdminDashboard, meta: { role: "admin" }},
-  { path: "/member", component: MemberDashboard, meta: { role: "member" }}
-]
+import { createRouter, createWebHistory } from 'vue-router'
+import AdminDashboard from '../views/AdminDashboard.vue'
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
-})
-
-router.beforeEach((to, from, next) => {
-  const auth = useAuthStore()
-
-  if (to.meta.role && auth.role !== to.meta.role) {
-    return next("/")
-  }
-  next()
+  history: createWebHistory('/fitlife-gym-website/'),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: AdminDashboard
+    }
+  ]
 })
 
 export default router
